43696e23d0ad9311798bc7d28e47fe1b811131f3
beastmatser3 <79206232+beastmatser3@users.noreply.github.com>
2021-04-18 20:18:42 +0200

    Update andere_commands.py
    Mod commands toegevoegd, door ratelimit issues (429 error), mod_commands.py verwijdert.

--- v2/andere_commands.py
+++ v2/andere_commands.py
@@ -63,6 +63,44 @@ async def sessie(ctx):
     embed = discord.Embed()
     embed.add_field(name="Helaas...", value=f"{ctx.author.name.split('[')[0]}, zonder de coach role kun je geen sessie maken! Als je toch iets wil doen kun je een ticket aanmaken ({ticket_channel.mention}).")
     await ctx.send(embed=embed)
+
+
+@client.command(aliases=["eenroles", "1role", "eenrole"])
+@commands.has_permissions(administrator=True)
+async def geen_roles(ctx):
+    channel1 = client.get_channel(788696271720415242)
+    channel2 = client.get_channel(818126902878404677)
+    members = [member for member in ctx.guild.members if len(member.roles) == 2 and not member.bot]
+    embed = discord.Embed(color=discord.Colour.lighter_grey())
+    embed.add_field(name="Roles", value=f"We hebben opgemerkt dat je enkel de [Ninja]/[Coach] (die heb je waarschijnlijk van een moderator gekregen). In {channel2.mention} kan je door onder de berichten op de emoticons te klikken de overeenkomende roles krijgen. Dit staat ook in de tutorial van de server: https://www.youtube.com/watch?v=eMDVAxBPnhM&feature=youtu.be\n\nAls je er niet uit kan geraken kun je nog altijd een ticket (in {channel1.mention}) aanmaken.\n\n**Dit is een geautomatiseerd bericht, antwoorden worden niet gelezen.**")
+    for member in members:
+        await member.send(embed=embed)
+    bericht_members = ', '.join([member.name for member in members])
+    await ctx.send(embed=discord.Embed(color=ctx.author.color).add_field(name="Command succesvol opgeroepen", value=f"Volgende member(s) kregen een bericht: {bericht_members}"))
+
+@client.command(aliases=["geefninja"])
+@commands.has_permissions(administrator=True)
+async def geef_ninja(ctx):
+    role_ninja = discord.utils.get(ctx.guild.roles, name="[Ninja]")
+    role_coach = discord.utils.get(ctx.guild.roles, name="[Coach]")
+    not_bots = [member for member in ctx.guild.members if not member.bot]
+    members = [member for member in not_bots if role_ninja not in member.roles and role_coach not in member.roles]
+    for member in members:
+        if not member.bot:
+            embed = discord.Embed()
+            embed.add_field(name=f"Hallo {member.name}!", value=f"We hebben gezien dat je nog geen roles hebt in de server, neem een kijkje in {client.get_channel(829633531579203595).mention} om daar jouw roles te krijgen, we hebben je alvast toegang gegeven tot de server \(:\n\n**Dit is een geautomatiseerd bericht, antwoorden worden niet gelezen.**")
+            await member.add_roles(role_ninja)
+            await member.send(embed=embed)
+        else:
+            pass
+    bericht_members = ', '.join([member.name for member in members])
+    embed = discord.Embed(color=ctx.author.color)
+    if not members:
+        embed.add_field(name="Command uitgevoerd, maar...", value="Iedereen heeft al een toepasselijke role (ninja, coach)")
+    else:    
+        embed.add_field(name="Command succesvol uitgevoerd", value=f"Volgende member(s) kregen de role [Ninja]: \n `{bericht_members}`")
+    await ctx.send(embed=embed)
+
   
 @client.event
 async def on_command_error(ctx, error):
