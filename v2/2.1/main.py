7eb3ca6d835f2b06c0ede8367a1f3f45bafe5ced
beastmatser3 <79206232+beastmatser3@users.noreply.github.com>
2021-04-26 17:19:39 +0200

    Indentatie verwijdert
    Indendatie verwijdert, custom bericht als een member de server toetreedt.

--- v2/2.1/main.py
+++ v2/2.1/main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
+from PIL import Image, ImageDraw, ImageFont
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix=".", intents=intents)
@@ -335,13 +336,31 @@ class MainCommands(commands.Cog):
         
     @commands.Cog.listener()
     async def on_member_join(self, member):
+        # welkom  bericht naar user
         introductie = self.client.get_channel(829633531579203595)
         regels = self.client.get_channel(790536868236099604)
         hoe_discord_gebruiken = self.client.get_channel(788694027359748106)
         maak_ticket_channel = self.client.get_channel(788696271720415242)
+        
         embed = discord.Embed(title="**Welkom**", description=f"Welkom bij de **CoderDojo Discord** server, {member.name}! Dit is de plek waar je jouw vragen kunt stellen over code gerelateerde zaken!\n\nOm van start te gaan moet je even een korte **vragenlijst** invullen in {introductie.mention}, zo komen we een beetje meer te weten over jouw kennen en kunnen.\n\nDe **regels** van de server moeten ten alle tijden worden gevolgd, deze regels kan je terugvinden in {regels.mention}.\n\nWe zouden ook heel graag hebben dat iedereen herkenbaar is, daarom zouden we je willen vragen om je **nickname**  te veranderen naar je voornaam gevolgd door de stad van jouw dojo tussen vierkante haakjes. Dit ziet er dan bv. zo uit: `Bart [Antwerpen]`. Heb je geen dojo kan je `Bart [Geen dojo]` neerplaatsen.\n\nAls je niet weet hoe dit moet of je begrijpt niet helemaal hoe Discord werkt staat er in {hoe_discord_gebruiken.mention} een **tutorial** die je kan bekijken.\n\nHeb je hulp nodig in verband met de Discord server, wil je feedback geven, een suggestie doen of iets melden over een bepaalde persoon? Je kan altijd een **ticket** aanmaken in {maak_ticket_channel.mention} en dan zal je zo snel mogelijk hulp krijgen.")
         embed.set_footer(text="Dit is een geautomatiseerd bericht, antwoorden worden niet gelezen.")
         await member.send(embed=embed)
+
+        # welkom bericht in kanaal
+        welkom_kanaal = self.client.get_channel(806505967390162944)
+        welkom_image = Image.open("welkom.png")
+
+        draw = ImageDraw.Draw(welkom_image)
+        lettertype = ImageFont.truetype("VAG_rounded_Light.ttf", 73)
+
+        draw.text((395, 15), f"{member.name}!", fill=(255, 255, 255), font=lettertype)
+        
+        welkom_image.save(f"{member.name}_welkom.png")
+        
+        await welkom_kanaal.send(file=discord.File(f"{member.name}_welkom.png"))
+        
+        os.remove(f"{member.name}_welkom.png")
+          
 
 
 
@@ -621,21 +640,21 @@ class SleepingChannels(commands.Cog):
 
 @client.event
 async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CommandOnCooldown):
+     if isinstance(error, commands.errors.CommandOnCooldown):
         seconden = error.retry_after % 60
-        minuten = (error.retry_after - seconden) / 60
+         minuten = (error.retry_after - seconden) / 60
         if minuten == 0:
-            await ctx.send(embed=discord.Embed(description=f"Wacht nog `{round(seconden, 1)}` om dit command te gebruiken in dit kanaal!"))
+             await ctx.send(embed=discord.Embed(description=f"Wacht nog `{round(seconden, 1)}` om dit command te gebruiken in dit kanaal!"))
         else:
-            await ctx.send(embed=discord.Embed(description=f"Wacht nog `{round(minuten)} min en {round(seconden, 1)}s` om dit command te gebruiken in dit kanaal!"))
+             await ctx.send(embed=discord.Embed(description=f"Wacht nog `{round(minuten)} min en {round(seconden, 1)}s` om dit command te gebruiken in dit kanaal!"))
     else:
-        pass
+         pass
 
 
 client.add_cog(TicketSystem(client))
 client.add_cog(ReactionRoles(client))
 client.add_cog(MainCommands(client))
-# client.add_cog(AndereCommands(client))
+client.add_cog(AndereCommands(client))
 client.add_cog(Comms(client))
 client.add_cog(SleepingChannels(client))
 
