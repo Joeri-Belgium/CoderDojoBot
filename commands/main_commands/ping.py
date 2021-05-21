8878832898167637043c66765073d503570af1a1
Joeri-Belgium <83272818+Joeri-Belgium@users.noreply.github.com>
2021-05-21 16:50:50 +0200

    Update ping.py
    added pong / pang als parodie

--- commands/main_commands/ping.py
+++ commands/main_commands/ping.py
@@ -15,5 +15,18 @@ class Ping(commands.Cog):
     async def ping(self, ctx):
         await self.embeds(ctx, "Ping", f"The delay of the bot is: **{round(self.client.latency * 1000)}ms**.")
 
+    @commands.command(aliases=["paf","poef","schoot","fire"],description='some fun command....')
+    async def pang(self, ctx):
+        await self.embeds(ctx, "PangPangPang",
+                          "Alle Indianen... schieten met 🍌🍌🍌...")
+
+    @commands.command(aliases=[],description='some extra fun command...')
+    async def pong(self, ctx):
+        await self.embeds(ctx, "Pong...",
+                          "Ping, pong anyone?")
+        
+        
+        
+        
 def setup(client):
     client.add_cog(Ping(client))
